f6e051f606d2a2d914405002435c2be42ec9731f
Holovko Maksym <golovkomaksim852@gmail.com>
2024-10-13 15:52:00 +0300

    Ux-improve

---
+++ assets/js/formHandler.js
@@ -0,0 +1,14 @@
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(this);
+
+  const formObject = {};
+  formData.forEach((value, key) => {
+    formObject[key] = value;
+  });
+
+  console.log(formObject);
+
+  this.reset();
+});
--- public/components/form.php
+++ public/components/form.php
@@ -49,18 +49,3 @@
         </form>
     </div>
 </section>
-
-<script>
-    document.getElementById('myForm').addEventListener('submit', function(event) {
-        event.preventDefault(); 
-
-        const formData = new FormData(this); 
-
-        const formObject = {};
-        formData.forEach((value, key) => {
-            formObject[key] = value;
-        });
-
-        console.log(formObject); 
-    });
-</script>
